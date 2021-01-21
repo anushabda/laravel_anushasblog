07e405e424ad9c41c9488576278355d0b28ca4cf
anushabda <anushabeatrice@gmail.com>
2021-01-22 10:25:37 +1100

    footer change

--- resources/views/user/layouts/footer.blade.php
+++ resources/views/user/layouts/footer.blade.php
@@ -1,9 +1,9 @@
 <footer class="py-5 mt-5" style="background-color:#330000">
     <div class="container">
       <p class="text-center" style="color:LightSalmon"><i>Slow and Steady will reach the <u>Finish Line</u></i></p>
-      <div id="footercontent"class="d-flex justify-content-between">
-      <div class="inline-block m-0 text-white">Copyright &copy; www.anushasblog.com</div>
-      <div class="inline-block m-0 text-right text-white">Contact me @ anushabeatrice@gmail.com</div>
+      <p class="text-center text-white">Copyright &copy; www.anushasblog.com</p> 
+     
+     
     </div>
     </div>
   </footer>
