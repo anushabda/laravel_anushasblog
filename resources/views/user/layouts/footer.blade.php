6b78f307c61f2295b8285824ca91547277ad5df2
anushabda <anushabeatrice@gmail.com>
2021-02-03 10:02:29 +1100

    view changes

--- resources/views/user/layouts/footer.blade.php
+++ resources/views/user/layouts/footer.blade.php
@@ -1,6 +1,7 @@
 <footer class="py-5 mt-5" style="background-color:#330000">
     <div class="container">
-      <p class="text-center" style="color:LightSalmon"><i>Slow and Steady will reach the <u>Finish Line</u></i></p>
+      <p class="text-center" style="color:LightSalmon"><i>Slow and Steady will reach the <u>Finish Line</u></i><br>
+      <i>Fast and Steady will <b>bring forth</b> the Finish Line</i></p>
       <p class="text-center text-white">Copyright &copy; www.anushasblog.com</p> 
      
      
